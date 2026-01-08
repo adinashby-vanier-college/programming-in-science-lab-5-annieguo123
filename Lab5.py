e310ea450b652ee22ab58eb795a68c7e6d089b84
annieguo123 <annieguo029@gmail.com>
2026-01-08 15:20:22 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,22 +1,91 @@
-# Example for n = 5:
+# Example for n = 5: USING WHILE LOOP
 # *****
 # *   *
 # *   *
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    if n <= 0:
+        return ""
 
+    result = ""
+    i = 0
+
+    while i < n:
+        if i == 0 or i == n - 1:
+            result += "*" * n
+        else:
+            if n == 1:
+                result += "*"
+            else:
+                result += "*" + " " * (n - 2) + "*"
+
+        if i != n - 1:
+            result += "\n"
+
+        i += 1
+
+    return result
+print(hollow_square(5))
+
+# USING WHILE LOOP:
 # 1
 # 12
 # 123
 # 1234
-def number_pattern(n):
-    return ""
 
-# Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
+# def number_pattern(n):
+    result =""
+    i = 1
+
+    while i <= n:
+        j = 1
+        while j <= i:
+
+            # result = result + str(j)
+            result += str(j)
+            j += 1
+
+        if i != n:
+            result += "\n"
+
+        i += 1
+
+    return result
+
+
+
+
+# Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15 USING A WHILE LOOP
+
 def sum_of_natural_numbers(n):
-    return ""
+    sum = 0
+    i = 0
+    while i <= n:
+        sum += i
+        i += 1
+    return sum
+
+print(sum_of_natural_numbers(5))
+
+
+# FOR LOOP:
+
+# def sum_of_natural_numbers(n):
+#     sum = 0
+#     for i in range(n+1):
+#         sum += i
+#     return sum
+# print(sum_of_natural_numbers(8))
+       
+
+
+
+
+
+
+
+
 
 # Example for n = 4:
 #    *
@@ -24,4 +93,11 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    if n <= 0:
+        return ""
+    result = ""
+    i = 0
+
+    while i < n:
+        if i == 0:
+        result += " " * (n-1) + "*" + " " * (n-1)
